37299a815892ff6e4fe3c22a938426c3ae944875
jacobruiz428 <jacobruiz428@gmail.com>
2026-03-26 15:59:54 -0700

    work

--- Main.java
+++ Main.java
@@ -1,7 +1,15 @@
-
+import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(150, 250);
+        frame.setTitle("testing");
+
+        testingShapeComponent component = new testingShapeComponent();
+        frame.add(component);
+
+        frame.setVisible(true);
         
     }
 }
